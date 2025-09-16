0bdf7a1bb082cfbdf5b0de9ea9e8609f5a7f2eb0
Tanvir <tanvir.info07@gmail.com>
2025-09-16 11:00:40 +0600

    initalise Dashboard app in vite-react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="bg-teal-400 p-4 text-white text-3xl font-bold underline text-center">
+        wellcome to MamarDokan
+      </p>
+    </>
+  );
+}
+
+export default App;
